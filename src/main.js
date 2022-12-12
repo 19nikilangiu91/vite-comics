3e8c4ba613c96fe3980ac6c7091b03d46c6f51c3
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 14:38:33 +0100

    FontAwesome Install

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// FontAwesome Install
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faUser)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
