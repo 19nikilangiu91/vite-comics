07450612089a80db2403ed8a6833a751dfaa950f
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 17:00:45 +0100

    Upgrade AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "AppMain",
+}
+
+</script>
+
+<template>
+    <main>
+        <div class="container-main">
+            <section class="main-content">
+                <h1>Add Content</h1>
+            </section>
+
+            <section class="main-loges"></section>
+
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    width: 100%;
+    // DEBUG
+    background-color: red;
+
+    .container-main {
+        width: 80%;
+        margin: 0 auto;
+        // DEBUG
+        background-color: yellow;
+    }
+}
+</style>
