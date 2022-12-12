2cc8be71bd4425acb16c41db7658fcee8e51fffc
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 14:39:06 +0100

    Debug FontAwesome

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
+      <font-awesome-icon icon="fa-solid fa-user" />
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
@@ -21,9 +22,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
