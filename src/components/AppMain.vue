9b3116bbb8cb8baf82c6ec27c330e62dfb5027f8
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 18:23:07 +0100

    Upgrade AppMain Data, Template and Style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,37 @@
 <script>
 export default {
     name: "AppMain",
+    data() {
+        return {
+            jumbotron: "./src/assets/img/jumbotron.jpg",
+            cards: [
+                {
+                    image: "./src/assets/img/buy-comics-digital-comics.png",
+                    content: "DIGITAL COMICS",
+
+                },
+                {
+                    image: "./src/assets/img/buy-comics-merchandise.png",
+                    content: "DC MERCHANDISE",
+                },
+                {
+                    image: "./src/assets/img/buy-comics-subscriptions.png",
+                    content: "SUBSCRPTION",
+
+                },
+                {
+                    image: "./src/assets/img/buy-comics-shop-locator.png",
+                    content: "COMIC SHOP LOCATOR",
+
+                },
+                {
+                    image: "./src/assets/img/buy-dc-power-visa.svg",
+                    content: "DC POWER VISA",
+
+                }
+            ]
+        }
+    }
 }
 
 </script>
@@ -9,11 +40,17 @@ export default {
     <main>
         <div class="container-main">
             <section class="main-content">
-                <h1>Add Content</h1>
+                <div>
+                    <img :src="jumbotron" alt="jumbotron" class="jumbotron">
+                </div>
             </section>
 
-            <section class="main-loges"></section>
-
+            <section class="main-loges">
+                <div v-for="(card, index) in cards" :key="index" class="card">
+                    <img :src="card.image" alt="">
+                    <h6>{{ card.content }}</h6>
+                </div>
+            </section>
         </div>
     </main>
 </template>
@@ -25,10 +62,43 @@ main {
     background-color: red;
 
     .container-main {
-        width: 80%;
+        width: 100%;
         margin: 0 auto;
-        // DEBUG
-        background-color: yellow;
+        background-color: #0282f9;
+
+        .main-content {
+            width: 100%;
+
+            .jumbotron {
+                width: 100%;
+                height: 500px;
+                background-size: cover;
+            }
+        }
+
+        .main-loges {
+            width: 70%;
+            margin: 0 auto;
+            display: flex;
+            justify-content: center;
+
+            .card {
+                display: flex;
+                justify-content: center;
+                width: 50%;
+                height: 50%;
+                margin: 10px 0;
+            }
+
+            img {
+                width: 45px;
+                height: 50px;
+            }
+
+            h6 {
+                margin-left: 10px;
+            }
+        }
     }
 }
 </style>
