fafab3afe0e442f8d3ac8ad636e66367f7c4e2ff
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 15:48:20 +0100

    Create App Header and Added Logo

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+
+</script>
+
+<template>
+    <header>
+        <a href="#">
+            <img src="../assets/img/dc-logo.png" alt="Dc-logo">
+        </a>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    margin: 0 auto;
+    width: 80%;
+}
+</style>
