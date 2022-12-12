346424d9688f6a84a50367dd18d2691f4cc1cb22
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-12 16:31:08 +0100

    Add Data and Header, Style contents

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,80 @@
 <script>
 export default {
     name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: "true",
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: "false",
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: "false",
+                }
+            ]
+        }
+    }
 }
 
 </script>
 
 <template>
     <header>
-        <a href="#">
-            <img src="../assets/img/dc-logo.png" alt="Dc-logo">
-        </a>
+        <nav>
+            <div class="logo">
+                <a href="#">
+                    <img src="../assets/img/dc-logo.png" alt="Dc-logo">
+                </a>
+            </div>
+            <li v-for="(link, index) in links" :key="index">
+                <a :href="link.url">
+                    {{ link.text }}
+                </a>
+            </li>
+        </nav>
     </header>
 </template>
 
@@ -17,5 +82,22 @@ export default {
 header {
     margin: 0 auto;
     width: 80%;
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .logo {
+            width: 50%;
+        }
+
+        li,
+        a {
+            list-style-type: none;
+            text-decoration: none;
+            margin: 3px;
+        }
+    }
 }
 </style>
