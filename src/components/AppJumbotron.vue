87473a5c4434f3f12ce4f7354f5e108399cb85d7
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-13 17:45:27 +0100

    Create AppJumbotron.vue

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            jumbotron: "./src/assets/img/jumbotron.jpg",
+        }
+    }
+}
+
+</script>
+
+<template>
+    <main>
+        <div class="container-main">
+            <section class="main-content">
+                <div>
+                    <img :src="jumbotron" alt="jumbotron" class="jumbotron">
+                </div>
+            </section>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    width: 100%;
+
+
+    .container-main {
+        width: 100%;
+        margin: 0 auto;
+        background-color: #1c1c1c;
+
+        .main-content {
+            width: 100%;
+
+            .jumbotron {
+                width: 100%;
+                height: 500px;
+                background-size: cover;
+            }
+        }
+    }
+}
+</style>
