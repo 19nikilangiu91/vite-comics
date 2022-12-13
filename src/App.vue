4aa3494f444cd9bd723089ebbcf809ad4e8f30cb
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-13 18:20:55 +0100

    Incollo data da Appheader a App

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,70 @@ export default {
     ListProduct,
     AppMain,
     AppFooter
+  },
+  data() {
+    return {
+
+      menu: [
+        {
+          text: "CHARACTERS",
+          url: "#",
+          current: "true",
+        },
+        {
+          text: "COMICS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "MOVIES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "GAMES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "COLLECTIBLES",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "VIDEOS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "FANS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "NEWS",
+          url: "#",
+          current: "false",
+        },
+        {
+          text: "SHOP",
+          url: "#",
+          current: "false",
+        }
+      ]
+    }
   }
 }
 
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader :links="menu" />
   <AppJumbotron />
   <ListProduct />
   <AppMain />
