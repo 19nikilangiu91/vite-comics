b6eaebcaa07371ff8983af92f4fcec72f5a21e0c
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-13 15:59:25 +0100

    Upgrade ProductCard

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+    name: "ProductCard",
+    promps: ["details"]
+}
+
+</script>
+
+<template>
+    <!-- Card -->
+    <div class="prod-card">
+        <img :src="details" alt="">
+        <h3>{{ details }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.prod-card {
+    margin: 5px;
+    width: calc((100% / 6) - 10px);
+
+    // background-color: blue
+    img {
+        width: 100%;
+        height: 250px;
+    }
+}
+</style>
